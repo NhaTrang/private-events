78997e402f9820adbd5ca834c697939da99ff3b2
Dan <dchen12812@gmail.com>
2015-06-22 02:00:34 -0700

    added initialization of sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :user, only: [:create,:show,:new]
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end

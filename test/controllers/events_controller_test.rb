876c0552db110d283d1a0b71d07fee9b741c6d5d
Dan <dchen12812@gmail.com>
2015-07-14 00:08:25 -0700

    added some tests

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -3,7 +3,32 @@ require 'test_helper'
 class EventsControllerTest < ActionController::TestCase
   def setup
     @event = events(:one)
+    @user = users(:one)
   end
 
+  test "should redirect create when not logged in" do
+    assert_no_difference "Event.count" do
+      post :create, event: {title: "cat", date: 19500510}
+    end
+    assert_redirected_to root_path
+  end
+  	
+  test "should redirect new when not logged in" do
+    get :new
+    assert_redirected_to root_path
+  end
+
+  test 'should get index' do
+  	get :index
+  	assert_response :success
+  end
+
+  test 'should redirect to events path after post and logged in' do
+    log_in_as(@user)
+    assert_difference "Event.count" do
+      post :create, event: {title: "cat", date: '1950-05-10', descript: "momo", creator_id: @user.id}
+    end
+    assert_redirected_to events_path
+  end
 
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = users(:one)
+  	@event = Event.new(title:"Mockingbird", descript: "robots", date: '20100510', creator_id: @user.id)
+  end
+
+  test "is valid" do
+  	assert @event.valid?
+  end
+
+  test 'title present' do
+  	@event.title = "  "
+  	assert_not @event.valid?
+  end
+
+  test 'date present' do
+  	@event.date = "  "
+  	assert_not @event.valid?
+  end
+
+  test 'past event' do
+  	@event_one = events(:one)
+  	assert Event.past.include?(@event_one)
+  end
+
+  test 'future event' do
+  	@event_two = events(:two)
+  	assert Event.upcoming.include?(@event_two)
+  end
 end
